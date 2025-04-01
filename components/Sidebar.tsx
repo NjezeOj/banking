f59c59d522197ce2d878f8b0f661ffc03dca97e4
njezeoj <njezeojinnaka@gmail.com>
2025-04-01 08:08:08 +0100

    feature: view plaidlink

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import Footer from './Footer'
+import PlaidLink from './PlaidLink'
 
 const Sidebar = ({
     user
@@ -50,7 +51,7 @@ const Sidebar = ({
                 )
             })}
 
-            USER
+            <PlaidLink  user={user} />
             </nav>
 
             <Footer
